8c2b14109884bcd20036880c4c570578058f9f71
danieltyukov <60662998+danieltyukov@users.noreply.github.com>
2021-02-24 16:19:18 +0200

    rename files addition

--- automation-tool.py
+++ automation-tool.py
@@ -6,10 +6,21 @@ import json
 import time
 
 class MyHandler(FileSystemEventHandler):
+    i = 1
     def on_modified(self, event):
+        new_name = "new_file_" + str(self.i) + ".txt"
+        
         for filename in os.listdir(folder_to_track):
+            for filename in os.listdir(folder_to_track):
+                file_exists = os.path.isfile(folder_destination + "/" + new_name)
+                
+                while file_exists:
+                    self.i += 1
+                    new_name = "new_file_" + str(self.i) + ".txt"
+                    file_exists = os.path.isfile(folder_destination + "/" + new_name)
+            
             src = folder_to_track + "/" + filename
-            new_destination = folder_destination + "/" + filename
+            new_destination = folder_destination + "/" + new_name
             os.rename(src, new_destination)
     
 folder_to_track = "/Users/user/Desktop/myFolder"
